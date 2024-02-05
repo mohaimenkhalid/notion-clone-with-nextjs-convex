261dbbda9766500df1285b3d628b4d41b6c26415
mohaimen <mohaimen.jatri@gmail.com>
2024-02-06 01:43:14 +0600

    added hero section

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,9 +1,18 @@
-import { Button } from "@/components/ui/button"
+"use client"
 
-export default function Home() {
-  return (
-  <div>
-    <Button className="bg-emerald-500 text-white">Button</Button>
-  </div>
-  );
+import Heading from "./_components/heading";
+import Heroes from "./_components/heroes";
+import {Footer} from "@/app/(marketing)/_components/footer";
+
+export default function MarketingPage() {
+    return (
+        <div className="min-h-full flex flex-col dark:bg-[#1F1F1F]">
+            <div
+                className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
+                <Heading/>
+                <Heroes/>
+            </div>
+            <Footer />
+        </div>
+    );
 }
