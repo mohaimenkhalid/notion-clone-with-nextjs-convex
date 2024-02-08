b08b1575685e5ca09f63a8ddaa0469a31d9937bd
mohaimen <mohaimen.jatri@gmail.com>
2024-02-08 17:52:32 +0600

    added loading component

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -2,8 +2,13 @@ import React from 'react';
 import Link from "next/link";
 import {ArrowRight} from "lucide-react";
 import {Button} from "@/components/ui/button";
+import {SignInButton, UserButton} from "@clerk/clerk-react";
+import {useConvexAuth} from "convex/react";
+import {Spinner} from "@/components/spinner";
+
 
 function Heading() {
+    const { isAuthenticated, isLoading } = useConvexAuth();
     return (
         <div className="max-w-3xl space-y-4">
             <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
@@ -13,12 +18,30 @@ function Heading() {
                 Jotion is the connected workspace where <br/>
                 better, faster work happens.
             </h3>
-            <Button asChild>
-                <Link href="/documents">
-                    Enter Jotion
-                    <ArrowRight className="h-4 w-4 ml-2" />
-                </Link>
-            </Button>
+            {isLoading && (
+                <div className="w-full flex justify-center"><Spinner /></div>
+            )}
+            {!isAuthenticated && !isLoading && (
+                <>
+                    <SignInButton mode="modal">
+                        <Button size="sm">
+                            Get Jotion free
+                        </Button>
+                    </SignInButton>
+                </>
+            )}
+            {isAuthenticated && !isLoading && (
+                <>
+                    <Button variant="ghost" size="sm" asChild>
+                        <Link href="/documents">
+                            Enter Jotion
+                        </Link>
+                    </Button>
+                    <UserButton
+                        afterSignOutUrl="/"
+                    />
+                </>
+            )}
             {/*{isLoading && (
                 <div className="w-full flex items-center justify-center">
                     <Spinner size="lg" />
