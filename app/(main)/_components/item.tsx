085a310dc2a4fd379944d0fbb6ed977f757aa386
mohaimen <mohaimen707@gmail.com>
2024-02-29 02:28:45 +0600

    nested expand added

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -1,10 +1,14 @@
 "use client"
 
 import React from 'react';
-import {ChevronDown, ChevronRight, LucideIcon} from "lucide-react";
+import {ChevronDown, ChevronRight, LucideIcon, Plus} from "lucide-react";
 import {Id} from "@/convex/_generated/dataModel";
 import {cn} from "@/lib/utils";
 import {Skeleton} from "@/components/ui/skeleton";
+import {useRouter} from "next/navigation";
+import { useMutation } from "convex/react";
+import {api} from "@/convex/_generated/api";
+
 
 interface ItemProps {
     id?: Id<"documents">;
@@ -31,11 +35,33 @@ const Item = ({
                   onExpand,
                   expanded,
               }: ItemProps) => {
+    const router = useRouter();
+    const create = useMutation(api.documents.create);
+
     const ChevronIcon = expanded ? ChevronDown : ChevronRight;
     const handleExpand = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
         event.stopPropagation()
         onExpand?.()
     }
+    const onCreate = (
+        event: React.MouseEvent<HTMLDivElement, MouseEvent>
+    ) => {
+        event.stopPropagation();
+        if (!id) return;
+        const promise = create({ title: "Untitled", parentDocument: id })
+            .then((documentId) => {
+                if (!expanded) {
+                    onExpand?.();
+                }
+                router.push(`/documents/${documentId}`);
+            });
+
+        toast.promise(promise, {
+            loading: "Creating a new note...",
+            success: "New note created!",
+            error: "Failed to create a new note."
+        });
+    };
     return (
         <div
             onClick={onClick}
@@ -70,14 +96,25 @@ const Item = ({
                     <span className="text-xs">⌘</span>K
                 </kbd>
             )}
+            {
+                !!id && (
+                    <div
+                        role="button"
+                        onClick={onCreate}
+                        className="opacity-0 group-hover:opacity-100 h-full ml-auto rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600"
+                    >
+                        <Plus className="h-4 w-4 text-muted-foreground"/>
+                    </div>
+                )
+            }
         </div>
     );
 };
 
-Item.Skeleton = function ItemSkeleton({ level }: { level?: number}) {
+Item.Skeleton = function ItemSkeleton({level}: { level?: number }) {
     return (
         <div style={{paddingLeft: level ? `${(level * 12) + 25}px` : "12px"}}
-            className="flex gap-x-2 py-[3px]"
+             className="flex gap-x-2 py-[3px]"
         >
             <Skeleton className="h-4 w-4" />
             <Skeleton className="h-4 w-[30%]" />
